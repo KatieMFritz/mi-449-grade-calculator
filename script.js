bb167eebbd472ea1bd9002ad837ff88055f45a12
Katie Fritz <katie@katiemfritz.com>
2018-03-12 15:42:12 -0400

    make better use of moment.js, add localstorage

--- script.js
+++ script.js
@@ -18,33 +18,89 @@ var pointsStillNeededDisplay = document.getElementById('points-still-needed')
 
 
 // Set some dates
-var startDate = new Date(2018,00,09)
-var endDate = new Date(2018,04,01)
-var today = new Date()
-var todayFormatted = moment(today).format('YYYY-MM-DD')
+var startDate = moment("2018-01-09")
+var endDate = moment("2018-05-01")
+var today = moment()
+var todayFormatted = moment().format('YYYY-MM-DD')
 
+// Other global variables
 var pointsNeeded
+var targetDate
+var currentGrade
+
+var loadSaved = function() {
+    // If stored target date exists, use it
+    var storedTargetDate = window.localStorage.getItem('storedTargetDate')
+    if (storedTargetDate !== null) {
+      targetDate = storedTargetDate
+      targetDateInput.value = moment(targetDate).format('YYYY-MM-DD')
+    // Otherwise, use defaults
+    } else {
+      targetDateInput.value = todayFormatted
+    }
+
+    // If stored grade exists, use it
+    var storedGrade = window.localStorage.getItem('storedGrade')
+    if (storedGrade !== null) {
+      currentGrade = storedGrade
+      currentGradeInput.value = currentGrade
+    // Otherwise, use defaults
+    } else {
+      currentGradeInput.value = 0.0
+    }
+
+    // If stored projects data exists, use it
+    var stored09 = window.localStorage.getItem('stored09')
+    if (stored09 !== null) {
+      x09Projects.value = stored09
+    }
+    var stored19 = window.localStorage.getItem('stored19')
+    if (stored19 !== null) {
+      x19Projects.value = stored19
+    }
+    var stored28 = window.localStorage.getItem('stored28')
+    if (stored28 !== null) {
+      x28Projects.value = stored28
+    }
+
+
+    // Run other display functions if there is stored data
+    if ((storedGrade !== null) && (storedTargetDate !== null)) {
+      getTargets()
+      updateProjectMath()
+    }
+}
+
+// Clear data
+var clearData = function() {
+  localStorage.clear()
+}
 
 // Set the target grade and other plan info
 var getTargets = function() {
-  // Calculate the number of days into the course
-  var targetDate = new Date(targetDateInput.value)
-  var oneDay = 24*60*60*1000 // hours*minutes*seconds*milliseconds
-  var targetDayNumber = Math.round(Math.abs((startDate.getTime() - targetDate.getTime())/(oneDay)))
-  var todayNumber = Math.round(Math.abs((startDate.getTime() - today.getTime())/(oneDay)))
-  todayDateDisplay.innerHTML = todayFormatted
+  // Get and save the target date
+  targetDate = moment(targetDateInput.value)
+  window.localStorage.setItem('storedTargetDate', targetDate)
 
-    // Calculate and show the target grade
+  // Get and save the current grade
+  currentGrade = currentGradeInput.value
+  window.localStorage.setItem('storedGrade', currentGrade)
+
+  // Calculate the number of days into the course for the target date
+  var targetDayNumber = targetDate.diff(startDate, 'days')
+  var todayNumber = today.diff(startDate, 'days')
+
+  // Calculate and show the target grade
   var percentageThroughCourse = Math.abs(targetDayNumber / 112)
   var targetGrade = Math.round(percentageThroughCourse * 400) / 100
   targetGradeDisplay.innerHTML = targetGrade
 
   // Calculate and show days remaining
   var daysRemaining = Math.abs(targetDayNumber - todayNumber)
+  todayDateDisplay.innerHTML = todayFormatted
   daysRemainingDisplay.innerHTML = daysRemaining
 
   // Calculate and show points needed
-  var currentGrade = currentGradeInput.value
   pointsNeeded = Math.round ((targetGrade - currentGrade)*100) / 100
   pointsNeededDisplay.innerHTML = pointsNeeded
 
@@ -56,6 +112,9 @@ var getTargets = function() {
   // Calculate and show points needed per day
   var pointsPerDay = Math.round(pointsNeeded / daysRemaining * 100) / 100
   pointsPerDayDisplay.innerHTML = pointsPerDay
+
+  // and also update project math when this changes
+  updateProjectMath()
 }
 
 var updateProjectMath = function() {
@@ -66,12 +125,14 @@ var updateProjectMath = function() {
     /100
   totalPointsDisplay.innerHTML = totalPoints
   pointsStillNeededDisplay.innerHTML = Math.round((pointsNeeded - totalPoints)*100)/100
+
+  window.localStorage.setItem('stored09', x09Projects.value)
+  window.localStorage.setItem('stored19', x19Projects.value)
+  window.localStorage.setItem('stored28', x28Projects.value)
 }
 
 // Set dates when the page loads
-window.onload = function() {
-  targetDateInput.value = todayFormatted
-}
+window.onload = loadSaved()
 
 // Update when things change
 updateForm.addEventListener('click', getTargets)
